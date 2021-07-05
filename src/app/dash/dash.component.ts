245ece50a26436759b423d765ceffb394c6ddf3a
Jacek Kowalski <kicaj29@wp.pl>
2021-07-05 20:56:52 +0200

    used minicard

--- src/app/dash/dash.component.ts
+++ src/app/dash/dash.component.ts
@@ -9,7 +9,7 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 })
 export class DashComponent {
   /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  /*cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
@@ -26,6 +26,26 @@ export class DashComponent {
         { title: 'Card 3', cols: 1, rows: 2 },
         { title: 'Card 4', cols: 1, rows: 1 }
       ];
+    })
+  );*/
+
+  cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return {
+          columns: 1,
+          miniCard: { cols: 1, rows: 1 },
+          chart: { cols: 1, rows: 2 },
+          table: { cols: 1, rows: 4 },
+        };
+      }
+
+     return {
+        columns: 4,
+        miniCard: { cols: 1, rows: 1 },
+        chart: { cols: 2, rows: 2 },
+        table: { cols: 4, rows: 4 },
+      };
     })
   );
 
